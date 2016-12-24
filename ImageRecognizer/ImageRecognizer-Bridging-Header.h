8616f889be3eeff92ba3a9da3711a78f4d2f4803
ovechko.056@gmail.com <ovechko.056@gmail.com>
2016-12-24 19:11:52 +0200

    init

---
+++ ImageRecognizer/ImageRecognizer-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  ImageRecognizer-Bridging-Header.h
+//  ImageRecognizer
+//
+//  Created by user on 12/24/16.
+//  Copyright © 2016 alex.o. All rights reserved.
+//
+
+#ifndef ImageRecognizer_Bridging_Header_h
+#define ImageRecognizer_Bridging_Header_h
+
+
+#endif /* ImageRecognizer_Bridging_Header_h */
+
+#import "c_predict_api.h"
+#import "MxnetManager.h"
---
+++ ImageRecognizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageRecognizer
+//
+//  Created by user on 12/24/16.
+//  Copyright © 2016 alex.o. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
