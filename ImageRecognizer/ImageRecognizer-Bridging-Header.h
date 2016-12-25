c8bb259eca38eefdd4dc2504a8fbf90490dab84b
ovechko.056@gmail.com <ovechko.056@gmail.com>
2016-12-25 21:18:59 +0200

    fix project structure

--- ImageRecognizer/ImageRecognizer-Bridging-Header.h
+++ ImageRecognizer/ImageRecognizer-Bridging-Header.h
@@ -6,11 +6,5 @@
 //  Copyright © 2016 alex.o. All rights reserved.
 //
 
-#ifndef ImageRecognizer_Bridging_Header_h
-#define ImageRecognizer_Bridging_Header_h
 
-
-#endif /* ImageRecognizer_Bridging_Header_h */
-
-#import "c_predict_api.h"
 #import "MxnetManager.h"
