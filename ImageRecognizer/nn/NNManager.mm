5b249c442fbf937beff39e34ceeaf0466bc91606
Alex iOS <ovechko.056@gmail.com>
2019-02-23 14:53:15 +0200

    Refactoring.

--- ImageRecognizer/nn/NNManager.mm
+++ ImageRecognizer/nn/NNManager.mm
@@ -7,7 +7,6 @@
 #import "NNManager.h"
 
 #define pathToResource(path) [[NSBundle mainBundle] pathForResource: path ofType: nil]
-#define arrSize(arr)  sizeof(arr) / sizeof(arr[0])
 
 #define MEAN_R 117.0f
 #define MEAN_G 117.0f
@@ -22,7 +21,7 @@
 
 @interface NNManager () {
     PredictorHandle predictor;
-    NSMutableArray *model_synset;
+    NSMutableArray *modelSynset;
 }
 @end
 
@@ -45,11 +44,11 @@
         NSString *paramsPath    = pathToResource(@"params");
         NSString *synsetPath    = pathToResource(@"synset.txt");
                                     
-        NSString *model_symbol = [[NSString alloc] initWithData:[[NSFileManager defaultManager] contentsAtPath:jsonPath] encoding:NSUTF8StringEncoding];
-        NSData *model_params = [[NSFileManager defaultManager] contentsAtPath: paramsPath];
+        NSString *modelSymbol = [[NSString alloc] initWithData:[[NSFileManager defaultManager] contentsAtPath:jsonPath] encoding:NSUTF8StringEncoding];
+        NSData *modelParams = [[NSFileManager defaultManager] contentsAtPath: paramsPath];
         
         //loading synset...
-        model_synset = [NSMutableArray new];
+        modelSynset = [NSMutableArray new];
         NSString* synsetText = [NSString stringWithContentsOfFile:synsetPath
                                                          encoding:NSUTF8StringEncoding error:nil];
         NSArray* lines = [synsetText componentsSeparatedByCharactersInSet:
@@ -57,21 +56,21 @@
         for (NSString *l in lines) {
             NSArray *parts = [l componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
             if ([parts count] > 1) {
-                [model_synset addObject:[parts subarrayWithRange:NSMakeRange(1, [parts count]-1)]];
+                [modelSynset addObject:[parts subarrayWithRange:NSMakeRange(1, [parts count]-1)]];
             }
         }
         
         //predictor params
-        NSString *input_name = @"data";
-        const char *input_keys[1];
-        input_keys[0] = [input_name UTF8String];
-        const mx_uint input_shape_indptr[] = {0, 4};
+        NSString *inputName = @"data";
+        const char *inputKeys[1];
+        inputKeys[0] = [inputName UTF8String];
+        const mx_uint inputShapeIndptr[] = {0, 4};
         //shape of input tensor, image -  (1 x 3 color channels x Width x Height)
-        const mx_uint input_shape_data[] = {1, kDefaultChannels, kDefaultWidth, kDefaultHeight};
+        const mx_uint inputShapeData[] = {1, kDefaultChannels, kDefaultWidth, kDefaultHeight};
     
-        bool modelsNotLoaded = model_symbol == nil || model_symbol.length == 0 || model_params == nil || model_synset.count == 0;
+        bool modelsDidntLoad = modelSymbol == nil || modelSymbol.length == 0 || modelParams == nil || modelSynset.count == 0;
         
-        if (modelsNotLoaded) {
+        if (modelsDidntLoad) {
             NSException *e = [NSException
                               exceptionWithName: @"NullPreTrainedModelException"
                               reason: @"*** Pre-trained model  is null, cannot load it! Check model name and path!"
@@ -80,13 +79,13 @@
         }
         
         //create predictor
-        MXPredCreate([model_symbol UTF8String],     // structure of network (json file)
-                     [model_params bytes],          // pre-trained model
-                     (int)[model_params length],
+        MXPredCreate([modelSymbol UTF8String],     // structure of network (json file)
+                     [modelParams bytes],          // pre-trained model
+                     (int)[modelParams length],
                      1, 0, 1,
-                     input_keys,
-                     input_shape_indptr,
-                     input_shape_data,
+                     inputKeys,
+                     inputShapeIndptr,
+                     inputShapeData,
                      &predictor);
         NSLog(@"mxnet predictor has been created...");
     }
@@ -115,45 +114,46 @@
         CGColorSpaceRelease(colorSpace);
         
         // Subtract the mean and copy to the input buffer
-        float *input_buffer = new float[numForComputing];
+        float *inputBuffer = new float[numForComputing];
         
         for (int i = 0; i < numForRendering; i += 4) {
             int j = i / 4;
-            input_buffer[0 * kDefaultWidth * kDefaultHeight + j] = (imageData[i + 0] & 0xFF) - MEAN_R; // red
-            input_buffer[1 * kDefaultWidth * kDefaultHeight + j] = (imageData[i + 1] & 0xFF) - MEAN_G; // green
-            input_buffer[2 * kDefaultWidth * kDefaultHeight + j] = (imageData[i + 2] & 0xFF) - MEAN_B; // blue
+            inputBuffer[0 * kDefaultWidth * kDefaultHeight + j] = (imageData[i + 0] & 0xFF) - MEAN_R; // red
+            inputBuffer[1 * kDefaultWidth * kDefaultHeight + j] = (imageData[i + 1] & 0xFF) - MEAN_G; // green
+            inputBuffer[2 * kDefaultWidth * kDefaultHeight + j] = (imageData[i + 2] & 0xFF) - MEAN_B; // blue
         }
         
         mx_uint *shape = nil;
-        mx_uint shape_len = 0;
-        MXPredSetInput(predictor, "data", input_buffer, numForComputing);
+        mx_uint shapeLen = 0;
+        MXPredSetInput(predictor, "data", inputBuffer, numForComputing);
         MXPredForward(predictor);
         
-        delete[] input_buffer;
+        delete[] inputBuffer;
         delete[] imageData;
         
-        MXPredGetOutputShape(predictor, 0, &shape, &shape_len);
+        MXPredGetOutputShape(predictor, 0, &shape, &shapeLen);
         
         //output tensor size
         mx_uint tt_size = 1;
-        for (mx_uint i = 0; i < shape_len; i++) {
+        for (mx_uint i = 0; i < shapeLen; i++) {
             tt_size *= shape[i];
         }
 
         std::vector<float> outputs(tt_size);
         MXPredGetOutput(predictor, 0, outputs.data(), tt_size);
         
-        size_t max_idx = std::distance(outputs.begin(), std::max_element(outputs.begin(), outputs.end()));
-        NSArray *result = [model_synset objectAtIndex:max_idx];
-        outputs.clear();
-        
-        if(result != nil) {
-            NSString * description = [result componentsJoinedByString:@" "];
-            
-            dispatch_async(dispatch_get_main_queue(), ^(){
-                callback(description);
-            });
+        size_t maxIdx = std::distance(outputs.begin(), std::max_element(outputs.begin(), outputs.end()));
+        if (modelSynset.count <= maxIdx || maxIdx < 0) {
+            callback(nil);
+            return;
         }
+        NSArray *result = [modelSynset objectAtIndex:maxIdx];
+        
+        NSString * description = [result componentsJoinedByString:@" "];
+        
+        dispatch_async(dispatch_get_main_queue(), ^(){
+            callback(description);
+        });
     });
 }
 
@@ -161,32 +161,32 @@
 - (void) visualizeMeanData: (void (^)(UIImage *meanImage)) callback {
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(){
         // Visualize the Mean Data
-        float model_mean[kDefaultImageSize];
+        float modelMean[kDefaultImageSize];
 
         NSString *meanPath  = pathToResource(@"mean_224.bin");
         
         NSData *meanData = [[NSFileManager defaultManager] contentsAtPath:meanPath];
-        [meanData getBytes:model_mean length:[meanData length]];
+        [meanData getBytes:modelMean length:[meanData length]];
         
-        std::vector<uint8_t> mean_with_alpha(kDefaultWidth * kDefaultHeight * (kDefaultChannels + 1), 0);
-        float *p_mean[3] = {
-            model_mean + kDefaultWidth * kDefaultHeight * 0,
-            model_mean + kDefaultWidth * kDefaultHeight * 1,
-            model_mean + kDefaultWidth * kDefaultHeight * 2
+        std::vector<uint8_t> meanWithAlpha(kDefaultWidth * kDefaultHeight * (kDefaultChannels + 1), 0);
+        float *pMean[3] = {
+            modelMean + kDefaultWidth * kDefaultHeight * 0,
+            modelMean + kDefaultWidth * kDefaultHeight * 1,
+            modelMean + kDefaultWidth * kDefaultHeight * 2
         };
         
-        for (int i = 0, map_idx = 0, glb_idx = 0; i < kDefaultHeight; i++) {
+        for (int i = 0, mapIdx = 0, glbIdx = 0; i < kDefaultHeight; i++) {
             for (int j = 0; j < kDefaultWidth; j++) {
-                mean_with_alpha[glb_idx++] = p_mean[0][map_idx]; // red
-                mean_with_alpha[glb_idx++] = p_mean[1][map_idx]; // green
-                mean_with_alpha[glb_idx++] = p_mean[2][map_idx]; // blue
-                mean_with_alpha[glb_idx++] = 0; // alpha
-                map_idx++;
+                meanWithAlpha[glbIdx++] = pMean[0][mapIdx]; // red
+                meanWithAlpha[glbIdx++] = pMean[1][mapIdx]; // green
+                meanWithAlpha[glbIdx++] = pMean[2][mapIdx]; // blue
+                meanWithAlpha[glbIdx++] = 0; // alpha
+                mapIdx++;
             }
         }
         
-        NSData *mean_data = [NSData dataWithBytes:mean_with_alpha.data() length:mean_with_alpha.size() * sizeof(float)];
-        CGDataProviderRef provider = CGDataProviderCreateWithCFData((__bridge CFDataRef)mean_data);
+        NSData *meanDataAlpha = [NSData dataWithBytes:meanWithAlpha.data() length:meanWithAlpha.size() * sizeof(float)];
+        CGDataProviderRef provider = CGDataProviderCreateWithCFData((__bridge CFDataRef)meanDataAlpha);
         CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
         // Creating CGImage from cv::Mat
         CGImageRef imageRef = CGImageCreate(kDefaultWidth,
